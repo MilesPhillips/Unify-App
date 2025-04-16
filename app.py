7db07d6fcc365f1ed93ecf04908749a5227e7df9
MilesPhillips <miles.phillips64@gmail.com>
2025-04-15 18:27:50 -0700

    home

--- app.py
+++ app.py
@@ -4,7 +4,10 @@ from flask import render_template
 app = Flask(__name__)
 @app.route('/home')
 def home_page():
-	return render_template('home.html')
+	return render_template('Home.html')
 
 if __name__ == '__main__':
 	app.run(debug=True)
+
+#need a depth splash page to cusomize my web app, 
+# to take you to the home page, (http://127.0.0.1:5000/home)
